7067e40bec1e852afc5fab9460d0524369d767c6
guanhong97 <13414865286@163.com>
2021-04-18 09:16:53 +0800

    拦截器里实现jwt

--- src/main/java/com/lingnan/community/controller/UmsUserController.java
+++ src/main/java/com/lingnan/community/controller/UmsUserController.java
@@ -15,6 +15,8 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.lingnan.community.jwt.JwtUtil.USER_NAME;
+
 /**
  * @program: community
  * @description: 公告栏控制层
@@ -48,5 +50,11 @@ public class UmsUserController extends BaseController {
         Map<String, String> map = new HashMap<>(16);
         map.put("token", token);
         return ApiResult.success(map, "登录成功");
+    }
+
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public ApiResult<UmsUser> getUser(@RequestHeader(value = USER_NAME) String userName) {
+        UmsUser user = umsUserService.getUserByUsername(userName);
+        return ApiResult.success(user);
     }
 }
--- src/main/java/com/lingnan/community/mapper/UmsUserMapper.java
+++ src/main/java/com/lingnan/community/mapper/UmsUserMapper.java
@@ -9,26 +9,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UmsUserMapper extends BaseMapper<UmsUser> {
-    /**
-     * 注册功能
-     *
-     * @param dto
-     * @return 注册对象
-     */
-    UmsUser executeRegister(RegisterDTO dto);
-
-    /**
-     * 获取用户信息
-     *
-     * @param username
-     * @return dbUser
-     */
-    UmsUser getUserByUsername(String username);
-    /**
-     * 用户登录
-     *
-     * @param dto
-     * @return 生成的JWT的token
-     */
-    String executeLogin(LoginDTO dto);
 }
